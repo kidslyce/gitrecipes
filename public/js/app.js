2ce133aa2ad91a81303ac4ff61e2d9239abc06f9
Kellen  Chumley <kidslyce@gmail.com>
2020-10-06 13:27:15 -0500

    pulling to dev

--- public/js/app.js
+++ public/js/app.js
@@ -416,20 +416,20 @@ const Nav = (props) => {
             <span className="navbar-toggler-icon"></span>
           </button>
           <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Launch demo modal
+  Login
 </button>
 
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Add Recipe</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-      <AddRecipe />
+      <Login />
       
       </div>
       <div class="modal-footer">
