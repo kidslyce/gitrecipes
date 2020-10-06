5c8c1c128651abde911eafd7f97195a29fe50b22
masonsteeger94 <masonsteeger@gmail.com>
2020-10-06 18:49:42 -0500

    for pull

--- public/js/app.js
+++ public/js/app.js
@@ -23,24 +23,18 @@ class SearchBar extends React.Component {
         event.preventDefault();
         this.setState({search: event.target.value});
     }
-        let filteredTags = this.props.recipes.filter(recipe => {
-              // return recipe.tags.includes(this.state.search)
-            }
-        )
-        return(
-    //rendering items based on tag
-    //in navbar
-    //if the onChange event takes in arguments wrap the property in an anonymous
-            <form onSubmit={() => {this.props.handleSearchSubmit(event, filteredTags)}} className="form-inline my-2 my-lg-0">
+        // let filteredTags = this.props.recipes.filter(recipe => {
+        //       // return recipe.tags.includes(this.state.search)
+        //     }
+        // )
+        return <form onSubmit={() => {this.props.handleSearchSubmit(event, filteredTags)}} className="form-inline my-2 my-lg-0">
                 <input className="form-control mr-sm-2"
                        type="text"
                        value={this.state.search}
                        onChange={this.updateSearch}
-                       aria-label="Search"
-                />
+                       aria-label="Search"/>
                 <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-        )
+            </form>)
     }
  }
 
